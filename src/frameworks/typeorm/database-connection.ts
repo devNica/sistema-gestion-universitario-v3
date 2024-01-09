523a3c7c02ac31107cb2096a9a6a0902d0a46885
devNica <designsdevnicaragua@gmail.com>
2024-01-09 12:23:13 -0600

    feat(core): configuracion del ORM para conexion a base de datos

---
+++ src/frameworks/typeorm/database-connection.ts
@@ -0,0 +1,18 @@
+import { getDatabaseCredential } from '@core/configs/db-credential.config'
+import { DataSource } from 'typeorm'
+
+const db = getDatabaseCredential()
+
+export const appDataSource = new DataSource({
+  type: db.DB_DIALECT,
+  host: db.DB_HOST,
+  port: 5432,
+  username: db.DB_USER,
+  password: db.DB_PASSWORD,
+  database: db.DB_NAME,
+  synchronize: true,
+  logging: false,
+  entities: ['src/core/adapters/secondary/orm/typeorm/models/**/*.ts'],
+  subscribers: [],
+  migrations: []
+})
--- src/server.ts
+++ src/server.ts
@@ -5,13 +5,18 @@ import 'module-alias/register'
 import constants from '@core/shared/constants'
 import { ExpressHttpServerAdapter } from '@core/adapters/primary/express/express-server.adapter'
 import { appExpress, server } from '@core/configs/server.config'
+import { TypeOrmAdapter } from '@core/adapters/secondary/orm/typeorm/typeorm.adapter'
 
 const { SERVER_PORT } = constants
 
 void startServer()
 
 async function startServer (): Promise<void> {
-  console.log(process.env)
+  const db = new TypeOrmAdapter()
+
+  db.connect()
+    .then(() => { console.log('Database connected success') })
+    .catch(error => { console.error(error) })
 
   const httpServer = new ExpressHttpServerAdapter(SERVER_PORT, appExpress, server)
   await httpServer.start()
