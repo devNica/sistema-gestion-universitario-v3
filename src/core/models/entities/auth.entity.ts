24493bd4a2f8849bb968af38cc03d97c306695b7
devNica <designsdevnicaragua@gmail.com>
2024-02-06 13:48:57 -0600

    feat(core): agregar nuevas entidades

---
+++ src/core/models/entities/auth.entity.ts
@@ -0,0 +1,46 @@
+import { type UUID } from '../generic/custom-types.model'
+
+export interface RolEntity {
+  id: UUID
+  rol: string
+  users?: Pick<UserAccountEntity, 'id' | 'email' | 'state'>
+}
+
+export interface UserHasRoleEntity {
+  rolId: UUID
+  userId: UUID
+}
+
+export interface UserAccountEntity {
+  id: UUID
+  email: string
+  password: string
+  isRoot: boolean
+  state: boolean
+  createdAt: Date
+  updatedAt: Date
+  profileId: string
+  roles?: Pick<RolEntity, 'id' | 'rol'>
+}
+
+export interface ProfileInfoEntity {
+  id: UUID
+  firstname: string
+  lastname: string
+  birthdate: string
+  dni: string
+  nationality: string
+  address: string
+}
+
+export interface ProfilePicture {
+  profileId: UUID
+  fileId: UUID
+}
+
+export interface FileEntity {
+  filename: UUID
+  filetype: string
+  filesize: number
+  binary: Buffer
+}
